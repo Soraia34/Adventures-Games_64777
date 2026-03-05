2ad906cb9076748920a13a6c9d67c1480deeedb6
Soraia34 <soraiap037@gmail.com>
2026-03-05 23:28:58 +0000

    Ambiente e física do jogo
    Ambiente e física do jogo feita!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using  System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2) transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2) rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
